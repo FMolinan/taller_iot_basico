ed9dbcedf110b1f31a875b7c788bd27aa0c0f3be
Fmolinan <felipe.molinan@usach.cl>
2019-07-03 17:24:06 -0400

    Arreglo typo - segunda leccion

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ LED1 = 13
 SWITCH = 11
 
 io.setup(LED1, io.OUT)
-io.setup(Switch, io.IN)
+io.setup(SWITCH, io.IN)
 
 # RUTINA PRINCIPAL
 
 if __name__ == '__main__':
     while True:
-        switch_value = io.input(Switch)
+        switch_value = io.input(SWITCH)
         if switch_value == 0:
             io.output(LED1, True)
         else:
